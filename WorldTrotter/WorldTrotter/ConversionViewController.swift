dc7c48e60c3daee08a2507bf82fa3c63f1cf77b0
donieli <donie.leigh@gmail.com>
2017-09-14 17:52:34 +0800

    finish chap.3 and chap.4

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Donie on 2017/9/14.
+//  Copyright © 2017年 Donie. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    @IBOutlet var textField: UITextField!
+    
+    var fehrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fehrenheitValue = fehrenheitValue {
+            return fehrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    @IBAction func fehrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fehrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fehrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        
+        if replacementTextHasDecimalSeperator != nil {
+            if existingTextHasDecimalSeperator != nil {
+                return false
+            }
+        } else {
+            let digits = NSCharacterSet.decimalDigits
+            for char in string.unicodeScalars {
+                if !digits.contains(char) {
+                    return false
+                }
+            }
+        }
+        
+        return true
+    }
+}

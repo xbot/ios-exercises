cd8609b79f3ec04dba4b3badf527b6e5817532a2
donieli <donie.leigh@gmail.com>
2017-09-26 18:13:14 +0800

    half finished chap.5

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Donie on 2017/9/26.
+//  Copyright © 2017年 Donie. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
